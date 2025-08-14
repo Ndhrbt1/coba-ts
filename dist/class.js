e7de434d79c37887857ee58e8c9812aff36caa38
Ndhrbt1 <indahroad04@gmail.com>
2025-08-15 03:23:09 +0700

    feat: class

--- dist/class.js
+++ dist/class.js
@@ -1,3 +1,62 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+class Manusia {
+    nama;
+    umur;
+    //   nama: string;
+    //   umur: number;
+    constructor(nama, umur) {
+        this.nama = nama;
+        this.umur = umur;
+        (this.nama = nama), (this.umur = umur);
+    }
+    getName() {
+        return this.nama;
+    }
+    getUmur() {
+        return this.umur;
+    }
+    haloDek() {
+        console.log("i always loving u 'till rn");
+    }
+}
+class Mahasiswa extends Manusia {
+    fakultas;
+    jurusan;
+    angkatan;
+    constructor(fakultas, jurusan, angkatan) {
+        super("f----", 22);
+        (this.fakultas = fakultas),
+            (this.jurusan = jurusan),
+            (this.angkatan = angkatan);
+    }
+    haloDek() {
+        console.log("'till 7 years u still not loving me");
+    }
+}
+const mahasiswa = new Mahasiswa("edu", "gatau", 2022);
+console.log(mahasiswa.getName());
+console.log(mahasiswa.getUmur());
+console.log(mahasiswa.fakultas);
+console.log(mahasiswa.jurusan);
+console.log(mahasiswa.angkatan);
+mahasiswa.haloDek();
+class Gaji {
+    golongan;
+    bayaran;
+    nama;
+    umur;
+    constructor(golongan, bayaran, nama, umur) {
+        (this.golongan = golongan),
+            (this.bayaran = bayaran),
+            (this.nama = nama),
+            (this.umur = umur);
+    }
+    hitung() {
+        return this.golongan * this.bayaran;
+    }
+}
+const gaji = new Gaji(3, 5000000, "fresh graduate", 22);
+console.log(gaji);
+console.log(gaji.hitung());
 //# sourceMappingURL=class.js.map
---
+++ src/class.ts
@@ -0,0 +1,69 @@
+abstract class Manusia {
+  //   nama: string;
+  //   umur: number;
+
+  constructor(private nama: string, protected umur: number) {
+    (this.nama = nama), (this.umur = umur);
+  }
+
+  getName(): string {
+    return this.nama;
+  }
+  getUmur(): number {
+    return this.umur;
+  }
+
+  haloDek(): void {
+    console.log("i always loving u 'till rn");
+  }
+}
+
+class Mahasiswa extends Manusia {
+  fakultas: string;
+  jurusan: string;
+  angkatan: number;
+  constructor(fakultas: string, jurusan: string, angkatan: number) {
+    super("f----", 22);
+    (this.fakultas = fakultas),
+      (this.jurusan = jurusan),
+      (this.angkatan = angkatan);
+  }
+
+  override haloDek(): void {
+    console.log("'till 7 years u still not loving me");
+  }
+}
+const mahasiswa = new Mahasiswa("edu", "gatau", 2022);
+console.log(mahasiswa.getName());
+console.log(mahasiswa.getUmur());
+console.log(mahasiswa.fakultas);
+console.log(mahasiswa.jurusan);
+console.log(mahasiswa.angkatan);
+mahasiswa.haloDek();
+
+interface Dosen {
+  nama: string;
+  umur: number;
+}
+
+class Gaji implements Dosen {
+  golongan: number;
+  bayaran: number;
+  nama: string;
+  umur: number;
+
+  constructor(golongan: number, bayaran: number, nama: string, umur: number) {
+    (this.golongan = golongan),
+      (this.bayaran = bayaran),
+      (this.nama = nama),
+      (this.umur = umur);
+  }
+
+  hitung(): number {
+    return this.golongan * this.bayaran;
+  }
+}
+
+const gaji = new Gaji(3, 5000000, "fresh graduate", 22);
+console.log(gaji);
+console.log(gaji.hitung());
